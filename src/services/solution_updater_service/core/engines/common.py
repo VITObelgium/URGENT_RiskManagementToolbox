6b5d3598647ca3b47356ee7e4f1c4084ef843415
Marcin <117676070+MarcinStolarczyk01@users.noreply.github.com>
2025-05-09 21:26:27 +0200

    Log solution states (#40)
    Co-authored-by: Damian Janiga <61538076+dJaniga@users.noreply.github.com>

--- src/services/solution_updater_service/core/engines/common.py
+++ src/services/solution_updater_service/core/engines/common.py
@@ -1,10 +1,28 @@
+from __future__ import annotations
+
 from abc import ABC, abstractmethod
+from dataclasses import dataclass
 
 import numpy as np
 import numpy.typing as npt
 
+from services.solution_updater_service.core.utils.type_checks import ensure_not_none
+
+
+@dataclass(frozen=True)
+class SolutionMetrics:
+    global_min: float
+
+    last_population_min: float
+    last_population_max: float
+    last_population_avg: float
+    last_population_std: float
+
 
 class OptimizationEngineInterface(ABC):
+    def __init__(self) -> None:
+        self._metrics: SolutionMetrics | None = None
+
     @abstractmethod
     def update_solution_to_next_iter(
         self,
@@ -15,9 +33,32 @@ class OptimizationEngineInterface(ABC):
     ) -> npt.NDArray[np.float64]: ...
 
     @property
-    @abstractmethod
-    def global_best_result(self) -> float: ...
+    def metrics(self) -> SolutionMetrics:
+        return ensure_not_none(self._metrics)
+
+    @property
+    def global_best_result(self) -> float:
+        return self.metrics.global_min
 
     @property
     @abstractmethod
     def global_best_controll_vector(self) -> npt.NDArray[np.float64]: ...
+
+    def _update_metrics(self, new_results: npt.NDArray[np.float64]) -> None:
+        population_min = float(new_results.min())
+        population_max = float(new_results.max())
+        population_avg = float(np.average(new_results))
+        population_std = float(np.std(new_results))
+
+        if self._metrics is None:  # first run
+            global_min = population_min
+        else:
+            global_min = min(population_min, self._metrics.global_min)
+
+        self._metrics = SolutionMetrics(
+            global_min=global_min,
+            last_population_min=population_min,
+            last_population_max=population_max,
+            last_population_avg=population_avg,
+            last_population_std=population_std,
+        )

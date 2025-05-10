3cec27a985d766d48aba32542162044fa6886ba7
Hevagog <qkawiak@gmail.com>
2025-05-10 13:42:55 +0200

    ruff refactor & coverage report fix

--- src/logger/__init__.py
+++ src/logger/__init__.py
@@ -1,7 +1,9 @@
+from logger.numeric_logger import get_csv_logger
 from logger.orchestration_logger import log_docker_logs
 from logger.u_logger import configure_logger, get_logger
 
 __all__ = [
+    "get_csv_logger",
     "configure_logger",
     "get_logger",
     "log_docker_logs",
